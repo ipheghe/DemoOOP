5a818d48e383bf692b2d18b9985316962ab085a5
ovie <iphegheovie@yahoo.com>
2017-07-05 12:20:45 +0100

    updated project work

--- src/main.js
+++ src/main.js
@@ -1,116 +1,172 @@
-
 'use strict';
 
-(function(exports) {
-
-// Define an ArithemeticSuperClass constructor
-var ArithemeticSuperClass = function(firstNumber, secondNumber) {
+// Define a class called ArithemeticSuperClass
+class ArithemeticSuperClass {
     
-    if (typeof(firstNumber && secondNumber) !== 'number') {
-            
-            return 'undefined';
-            
-    }else{
-        this.firstNumber = firstNumber || 22;
+    constructor(firstNumber, secondNumber) { //constructor class containing two arguments
+        this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
     }
-
-};
- exports.ArithemeticSuperClass = ArithemeticSuperClass;
-
-//generic method to get sum of numbers
-ArithemeticSuperClass.prototype.getSum = function(){
-    return this.firstNumber + this.secondNumber;
-};
-
-//generic method to get difference of numbers
-ArithemeticSuperClass.prototype.getDifference= function(){
-    return this.firstNumber - this.secondNumber;
-};
-
-//generic method to get product of numbers
-ArithemeticSuperClass.prototype.getProduct = function(){
-    return this.firstNumber * this.secondNumber;
-};
-
-//generic method to get quotient of numbers
-ArithemeticSuperClass.prototype.getQuotient = function(){
-    return this.firstNumber / this.secondNumber;
-};
-
-//generic method to get quotient of numbers
-ArithemeticSuperClass.prototype.getQuotient = function(){
-    return this.firstNumber / this.secondNumber;
-};
-
-//COMPOSITION
-//COMPOSITION is how we express all our “has a” relationships
-//newNumber is the object and contains properties firstNumber,secondNumber and thirdNumber
-//numberObject is a property of newNumber and has its own properties like x and y
-//Also OBJECTS can have properties and methods.
-var newNumber = {
-        
-firstNumber: 1,
-secondNumber: 2,
-thirdNumber: 3,
-numberObject: {
-            
-        x: 1,
-        y: 2
-    }
-            
- }
-
-//generic method to get quotient of numbers
-ArithemeticSuperClass.prototype.getAverage = function(){
     
-    //ENCAPSULATION
-    //the variable object x cannot be accessed from the outside
-    //but the method getAverage can be accessed and return the value of the function
-    var x = 2;
-    return (this.firstNumber / this.secondNumber) / x; //ABSTRACTION happens here because the its only the return value that can be accessed from the outside
-};
+   get firstNumber() {
+       
+        return this._firstNumber;
+    }
+       
+   set firstNumber(value) {
+       
+        if (value === null) {
+
+                throw new Error('Null Value');
+
+        }       
+        else if (typeof value !== 'number') {
+
+                throw new Error('"firstNumber" must be a number');
+
+        }
+
+        this._firstNumber = value;
+   }  
+    
+    
+    get secondNumber() {
+       
+       return  this._secondNumber;
+    }
+    
+   set secondNumber(value) {
+       
+              
+        if (value === null) {
+
+                throw new newError('null value');
+
+        } 
+        else if (typeof value !== 'number') {
+
+                throw new Error('"secondNumber" must be a number');
+
+        }
+
+        this._secondNumber = value;
+   }
+    
+
+    //generic method to get sum of numbers
+    getSum(){
+        
+        return this.firstNumber + this.secondNumber;
+    }
+    
+    //generic method to get diference of numbers
+    getDifference(){
+        
+        return this.firstNumber - this.secondNumber;
+    }
+    
+    
+    //generic method to get product of numbers
+    getProduct(){
+        
+        return this.firstNumber * this.secondNumber;
+    }
+    
+    //generic method to get quotient of numbers
+    getQuotient(){
+        
+        return this.firstNumber / this.secondNumber ;
+    }
+    
+     get numberList() {
+        
+        //COMPOSITION
+        //COMPOSITION is how we express all our “has a” relationships
+        //newNumber is the object and contains properties firstNumber,secondNumber and thirdNumber
+        //numberObject is a property of newNumber and has its own properties like x and y
+        //Also OBJECTS can have properties and methods.
+        var newNumber = {
+        
+            firstNumber: 1,
+            secondNumber: 2,
+            thirdNumber: 3,
+            numberObject: {
+            
+                x: 1,
+                y: 2
+            }
+            
+        }
+         
+     }
+    
+}
 
 
-// Define the Arithemetic constructor
-function Arithemetic(firstNumber, secondNumber) {
-  // Call the parent constructor, making sure (using Function#call)
-  // that "this" is set correctly during the call
-  ArithemeticSuperClass.call(this, firstNumber,secondNumber);
-
-  // Initialize our Student-specific properties
-  //this.subject = subject;
-};
 
 // INHERITANCE
-// Create a Arithemetic.prototype object that inherits from ArithemeticSuperClass.prototype.
-// Note: A common error here is to use "new ArithemeticSuperClass()" to create the
-// Arithemetic.prototype. That's incorrect for several reasons, not least 
-// that we don't have anything to give ArithemeticSuperClass for the "firstNumber" and "secondNumber" 
-// argument. The correct place to call ArithemeticSuperClass is above, where we call 
-// it from Arithemetic.
-Arithemetic.prototype = Object.create(ArithemeticSuperClass.prototype); // See note below
-
-// Set the "constructor" property to refer to Arithemetic
- Arithemetic.prototype.constructor = Arithemetic;
-
-// Replace the "getProduct" method
-Arithemetic.prototype.getProduct = function(){
+// Define class Arithemetic which extends ArithemeticSuperClass
+// Arithemetic inherits from the ArithemeticSuperClass class
+class Arithemetic extends ArithemeticSuperClass {
     
-   return "The result is equal to " + this.firstNumber * this.secondNumber;
+    //POLYMORPHISM  
+    // This is an example of polymorphism. The Arithemetic Class's
+    // "product()" and "quotient()" method overrides that of the ArithemeticSuperClass Class
+    //generic method to get product of numbers
+    //// Replace the "product" method
+    getProduct(){
+        
+         return 'The result is ' + (this.firstNumber * this.secondNumber);
+    }
     
-};
+    
+    //generic method to get average of a first and second number
+    getAverage(){
+          
+        //ENCAPSULATION
+        //the variable object sound cannot be accessed from the outside
+        //but the method getAverage can be accessed and return the value of the object variable sound
+        var x = 2   
+    
+        return (this.firstNumber + this.secondNumber) / x ;//ABSTRACTION happens here because the its only the return value that can be accessed from the outside
+    }
+    
+}
 
-var array = [];
+module.exports = Arithemetic;
+var answer;
 
-// Example usage:
-var newObject = new Arithemetic(2,3);
-array.push(newObject.getSum());//INHERITANCE
-array.push(newObject.getDifference());//INHERITANCE
-array.push(newObject.getProduct());//POLYMORPHISM
-array.push(newObject.getQuotient());//INHERITANCE
-array.push(newObject.getAverage());//ENCAPSULATION & ABSTRACTION
-array;
+var obj = new Arithemetic(2,3);
+console.log(obj.getSum()); //INHERITANCE
+console.log(obj.getProduct()); // POLYMORPHISM
+console.log(obj.getAverage()); // ENCAPSULATION
 
-});
+if (obj.getSum()){
+    
+    answer = obj.getSum();
+}
+
+else if (obj.getDifference()){
+    
+    answer = obj.getDifference
+}
+
+else if (obj.getProduct()){
+    
+    answer = obj.getProduct();
+}
+
+else if (obj.getQuotient()){
+    
+    answer = obj.getQuotient();
+}
+
+else if (obj.getAverage()){
+    
+    answer = obj.getAverage();
+}
+
+answer;
+
+
 
